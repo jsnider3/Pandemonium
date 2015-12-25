94f255193846c5ed35b77d7a11a9fb12ef5dd8b3
Joshua Snider <josh@joshuasnider.com>
2015-12-24 23:48:45 -0500

    Fixed data.

--- board.py
+++ board.py
@@ -1,8 +1,15 @@
 import networkx as nx
 
+def load_board():
+  return nx.read_edgelist('board.dat', delimiter=',')
+
 if __name__ == '__main__':
-  board = nx.read_edgelist('board.dat', delimiter=',')
-  for node in board:
-    print board.neighbors(node)
+  board = load_board()
+  centrality = nx.eigenvector_centrality(board, max_iter=1000)
+  stuff = []
+  for node in centrality:
+    stuff.append([node, str(centrality[node]), str(len(board.neighbors(node)))])
+  for line in sorted(stuff, key=lambda x: x[1]):
+    print line
 
 
---
+++ tester.py
@@ -0,0 +1,56 @@
+import board
+import unittest
+
+class Tests(unittest.TestCase):
+
+  def neighbors(self):
+    ''' Check that everyone has the correct number of neighbors. '''
+  brd = board.load_board()
+  assert len(brd.neighbors('Hong Kong')) == 6
+  assert len(brd.neighbors('Bangkok')) == 5
+  assert len(brd.neighbors('Chennai')) == 5
+  assert len(brd.neighbors('Kolkata')) == 4
+  assert len(brd.neighbors('Delhi')) == 5
+  assert len(brd.neighbors('Ho Chi Minh')) == 4
+  assert len(brd.neighbors('Manila')) == 5
+  assert len(brd.neighbors('Jakarta')) == 4
+  assert len(brd.neighbors('Karachi')) == 5
+  assert len(brd.neighbors('Baghdad')) == 5
+  assert len(brd.neighbors('Taipei')) == 4
+  assert len(brd.neighbors('Mumbai')) == 3
+  assert len(brd.neighbors('Tehran')) == 4
+  assert len(brd.neighbors('Shanghai')) == 5
+  assert len(brd.neighbors('Istanbul')) == 6
+  assert len(brd.neighbors('Cairo')) == 5
+  assert len(brd.neighbors('Sydney')) == 3
+  assert len(brd.neighbors('Riyadh')) == 3
+  assert len(brd.neighbors('San Francisco')) == 4
+  assert len(brd.neighbors('Algiers')) == 4
+  assert len(brd.neighbors('Tokyo')) == 4
+  assert len(brd.neighbors('Paris')) == 5
+  assert len(brd.neighbors('Moscow')) == 3
+  assert len(brd.neighbors('Los Angeles')) == 4
+  assert len(brd.neighbors('Madrid')) == 5
+  assert len(brd.neighbors('Chicago')) == 5
+  assert len(brd.neighbors('Milan')) == 3
+  assert len(brd.neighbors('Seoul')) == 3
+  assert len(brd.neighbors('St. Petersburg')) == 3
+  assert len(brd.neighbors('Essen')) == 4
+  assert len(brd.neighbors('London')) == 4
+  assert len(brd.neighbors('Mexico City')) == 5
+  assert len(brd.neighbors('Osaka')) == 2
+  assert len(brd.neighbors('Beijing')) == 2
+  assert len(brd.neighbors('New York')) == 4
+  assert len(brd.neighbors('Khartoum')) == 4
+  assert len(brd.neighbors('Bogota')) == 5
+  assert len(brd.neighbors('Miami')) == 4
+  assert len(brd.neighbors('Washington')) == 4
+  assert len(brd.neighbors('Sao Paulo')) == 4
+  assert len(brd.neighbors('Montreal')) == 3
+  assert len(brd.neighbors('Lima')) == 3
+  assert len(brd.neighbors('Lagos')) == 3
+  assert len(brd.neighbors('Kinshasa')) == 3
+  assert len(brd.neighbors('Buenos Aires')) == 2
+  assert len(brd.neighbors('Johannesburg')) == 2
+  assert len(brd.neighbors('Santiago')) == 1
+
